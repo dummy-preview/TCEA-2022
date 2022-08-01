018c998ea4c3010e329d4bf18eec0c907c266023
dummy-preview <91373388+dummy-preview@users.noreply.github.com>
2022-08-01 17:34:41 +0700

    update modal

--- js/engine.js
+++ js/engine.js
@@ -218,5 +218,18 @@ $.each(instance, function(key, value) {
             prevArrow.removeClass('disabled');
         }
     }
+});
 
+$('[open-modal]').on('click', function() {
+    var id = $(this).attr('open-modal');
+    $('.modal#' + id).addClass('active');
+});
+
+$('[close-modal]').on('click', function() {
+    $(this).parents('.modal').removeClass('active');
+});
+
+$('.modal').on('click', function(e) {
+    if (e.target !== this) { return };
+    $(this).removeClass('active');
 });
